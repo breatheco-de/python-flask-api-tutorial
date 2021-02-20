4e9d9ac5e4e85b33dc47369c9b9ccbc6beaa6155
Alejandro Sanchez <aalejo@gmail.com>
2021-02-20 00:16:38 -0500

    Update test.py

--- .learn/exercises/03.1-app-run/test.py
+++ .learn/exercises/03.1-app-run/test.py
@@ -10,5 +10,5 @@ def isOpen(ip,port):
       return False
 
 @pytest.mark.it("Make sure your app server is running, did you run the app.py?")
-def test_import_hello(config):
+def test_import_hello():
   assert isOpen('0.0.0.0', 3245)
