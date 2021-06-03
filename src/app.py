272634465e0fa96e67628cc206dcfe2446cb1b60
Kty <katiuska.rodriguez5@gmail.com>
2021-06-03 15:19:59 +0000

    cambios linea 8

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/hello', methods=['GET'])
 def hello_world():
-    return '<Hello!>'
-
+    return "<h1>Hello!</h1>"
+@app.route('/todos', methods=['GET'])
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
