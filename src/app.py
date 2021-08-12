1c40f5f28ae8e28c90292ee238ca4783116a16d9
Kty <katiuska.rodriguez5@gmail.com>
2021-08-12 13:08:54 +0000

    ktyprog/python-flask-api-tutorial.git

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/hello', methods=['GET'])
-def hello_world():
-    return "<h1>Hello!</h1>"
 @app.route('/todos', methods=['GET'])
+def hello_world():
+    retur <h1>Hello!</h1>
 
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
