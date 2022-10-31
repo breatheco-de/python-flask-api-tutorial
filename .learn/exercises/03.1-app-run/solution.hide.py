07e903bfdbeaee8b81210187f413a1815e2cfe43
ErnestoXG <ernestox.061@gmail.com>
2022-10-31 17:41:53 +0000

    Added first 3 solution files

---
+++ .learn/exercises/03.1-app-run/solution.hide.py
@@ -0,0 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
