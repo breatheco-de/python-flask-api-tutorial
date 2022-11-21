ca6efad83d3c4c0044daa83ae7333654727df320
ErnestoXG <ernestox.061@gmail.com>
2022-11-21 21:36:13 +0000

    Finalize all solutions

--- .learn/exercises/05-returning-json/solution.hide.py
+++ .learn/exercises/05-returning-json/solution.hide.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+todos = [ { "label": "My first task", "done": False } ]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return "<h1>Hello!</h1>"
+    return jsonify(todos)
 
 # These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
