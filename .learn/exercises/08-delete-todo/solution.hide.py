238789e9cf344b0f125ef3f4d042e45f9ed62158
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2024-01-11 09:09:05 +0100

    Update solution.hide.py

--- .learn/exercises/08-delete-todo/solution.hide.py
+++ .learn/exercises/08-delete-todo/solution.hide.py
@@ -16,10 +16,10 @@ def add_new_todo():
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete: ",position)
+    print("This is the position to delete:", position)
     return 'something'
 
 
-# These two lines should always be at the end of your app.py file.
+# These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
