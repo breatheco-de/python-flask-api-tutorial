ccf239134cc81ba08e435d590b00540745274591
Joao Nuñez <104280636+joaonunez@users.noreply.github.com>
2024-08-14 16:45:51 +0000

    Pending changes exported from your codespace

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "Comer", "done": False },
+    { "label": "Ducharse", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+
+def add_new_todo():
+   request_body = request.json
+   print("Peticion entrante ", request_body)
+   todos.append(request_body)
+   return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+
+def delete_todo(position):
+   print("Esta es la posicion para borrar", position)
+   todos.pop(position)
+   return jsonify(todos)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
