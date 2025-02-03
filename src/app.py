302996bd08b16fcf1e709c61162412e4802eecbc
Jujugm <julia7backup@gmail.com>
2025-02-03 09:29:31 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -1,27 +1,34 @@
+# Add the jsonify method to your Flask import
 from flask import Flask, jsonify, request
-app = Flask(__name__)
 
-todos = [ { "label": "My first task", "done": False } ]
+app = Flask(__name__)
+# Suppose you have your data in the variable named todos
+
+todos = [ 
+    { "label": "my first task", "done": False },
+  ]
+
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return jsonify(todos)
+    # You can convert that variable into a json string like this
+    json_text = jsonify(todos)
+
+    # And then you can return it to the front end in the response body like this
+    return json_text
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    todos.append(request_body)
+    todos.append (request_body)
     return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-    print("This is the position to delete:", position)
     del todos[position]
-    
+    print("This is the position to delete:", position)
     return jsonify(todos)
-# The request DELETE /todos/1 will call the function 'delete_todo' with the variable 'position == 1'
-# The request DELETE /todos/323 will call the function 'delete_todo' with the variable 'position == 323'
 
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
